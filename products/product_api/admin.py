90abf1ff33b4b9f826d611802363f6312876b89a
Alexsandr12 <magasov94@list.ru>
2022-12-14 15:32:55 +0400

    Add adminmodels setting.

--- products/product_api/admin.py
+++ products/product_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products, StatusProduct
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("article", "name_product", "price", "image", "status_id")
+    fields = ("article", "name_product", "price", "image", "status")
+
+
+class StatusProductAdmin(admin.ModelAdmin):
+    list_display = ("status",)
+    fields = ("status",)
+
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(StatusProduct, StatusProductAdmin)
